9da5f7345d4a09123341597fbe3cc17ca94f73ac
EHyang <xodid0936@gmail.com>
2020-10-05 13:56:31 +0900

    modify

--- test.js
+++ test.js
@@ -8,19 +8,20 @@ var app = express();
 
 app.set('port', 3000);
 
-app.get('/test', function(req,res) {
-  const excelFile = xlsx.readFile( "list.xls" );
+app.get('/test', function(req, res) {
+  const excelFile = xlsx.readFile("list.xls");
   const sheetName = excelFile.SheetNames[0];
   const firstSheet = excelFile.Sheets[sheetName];
 
-  const jsonData = xlsx.utils.sheet_to_json(firstSheet, {defval : ""});
+  const jsonData = xlsx.utils.sheet_to_json(firstSheet, {
+    defval: ""
+  });
   var text = "";
   text += "<html> <head> <title>z</title> </head> <body> <table border=3>";
   text += "<th>시도</th><th>의료기관명</th><th>주소</th>";
-  for(var i = 0; i<jsonData.length; i++)
-  {
+  for (var i = 0; i < jsonData.length; i++) {
     text += "<tr>";
-    text += "<td>" + jsonData[i]['시도'] +"</td>";
+    text += "<td>" + jsonData[i]['시도'] + "</td>";
     text += "<td>" + jsonData[i]['의료기관명'] + "</td>";
     text += "<td>" + jsonData[i]['주소'] + "</td>";
     text += '</tr>';
@@ -29,30 +30,34 @@ app.get('/test', function(req,res) {
   res.send(text);
 });
 
-app.get('/list',function(req,res){
+app.get('/list', async function(req, res) {
+  let ulList = [];
 
-  const getHtml = async () =>{
-    try{
-      return await axios.get("https://www.mohw.go.kr/react/ncov/selclinic04ls.jsp");
-    } catch (error){
-      console.error(error);
-    }
-  };
+  for (var i = 0; i < 31; i++) {
+    const getHtml = async () => {
+      try {
+        return await axios.get("https://www.mohw.go.kr/react/ncov/selclinic04ls.jsp?page=" + encodeURIComponent(i));
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-  getHtml().then(html => {
-    let ulList = [];
-    const $ = cheerio.load(html.data);
-    const $bodyList = $("table.co_tb_base tbody.tb_center").children("tr");
+    getHtml().then(html => {
+      const $ = cheerio.load(html.data);
+      const $bodyList = $("table.co_tb_base tbody.tb_center").children("tr");
 
-    $bodyList.each(function(i,elem){
-      ulList[i] = {
-        name : $(this).find('td.name strong').text()
-      };
+      $bodyList.each(function(i, elem) {
+        ulList[i] = {
+          name: $(this).find('td.name strong').text()
+        };
+      });
+
+
+        const data = ulList.filter(n => n.name);
+        res.json(data);
     });
+  }
 
-    const data = ulList.filter(n => n.name);
-    res.json(data);
-  });;
 
 
 });
